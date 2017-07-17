53ad1bb42bf23cf218fb2d38b02f01a23c8fc963
Kevin Thomas <kevint@magenic.com>
2017-07-17 14:36:31 -0500

    Adding to home page

--- AddieC/src/app/home/home.component.ts
+++ AddieC/src/app/home/home.component.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import * as firebase from 'firebase';
+import { UserService } from '../user/userShared/user.service';
+import { Post } from '../user/userShared/post';
 
 @Component({
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
     addieUrl = '../assets/addie.jpg';
+    posts: Post[];
+
+    constructor(private router: Router, private userSVC: UserService) {}
+
+    ngOnInit() {
+        this.getPosts();
+    }
+
+    getPosts() {
+        const dbRef = firebase.database().ref('/posts');
+        dbRef.once('value')
+            .then((snapshot) => {
+                const tmp: string[] = snapshot.val();
+                this.posts = Object.keys(tmp).map(key => tmp[key]);
+            });
+    }
+
+    choosePost(post: Post) {
+        this.router.navigate(['/post', post.id]);
+    }
+
+    getUser(id: number) {
+        
+    }
 }
---
+++ AddieC/src/app/postDetail/post-detail.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import * as firebase from 'firebase';
+import { Post } from '../user/userShared/post';
+
+@Component({
+    templateUrl: './post-detail.component.html',
+    styleUrls: ['./post-detail.component.css']
+})
+
+export class PostDetailComponent implements OnInit {
+    singlePost: Post;
+
+    constructor(private route: ActivatedRoute, private router: Router) {}
+
+    ngOnInit() {
+        const postId = this.route.snapshot.params['id'];
+        this.getSingle(postId);
+    }
+
+    getSingle(id: string) {
+        const dbRef = firebase.database().ref('/posts');
+        dbRef.orderByChild('id')
+            .equalTo(id) //equalTo needs an orderBy() to work properly
+            .once('value')
+            .then((snapshot) => {
+                const tmp = snapshot.val();
+                const transform = Object.keys(tmp).map(key => tmp[key]);
+                const title = transform[0].title;
+                const content = transform[0].content;
+                const uid = transform[0].uid;
+                const imgTitle = transform[0].imgTitle;
+                const img = transform[0].img;
+                this.singlePost = new Post(title, content, uid, imgTitle, img)
+            })
+    }
+}
